0f3f108e897cb8c403d954f0b989cae7064d58ef
Pharakit <mr.job4399@gmail.com>
2023-08-30 09:26:46 +0700

    aa

--- pages/dashboard/frmuser.js
+++ pages/dashboard/frmuser.js
@@ -89,7 +89,7 @@ export default function Component({ posts }) {
     console.log("password:", jsonData.password);
     console.log("status:", jsonData.status);
 
-     fetch('https://d61f-223-24-162-173.ngrok-free.app/api/users', {
+     fetch('https://5bc4-223-24-162-173.ngrok-free.app/api/users', {
         method: 'POST', // or 'PUT'
         headers: {
           'Content-Type': 'application/json',
